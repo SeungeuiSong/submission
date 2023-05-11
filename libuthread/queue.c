f71e845725c428b3c23819e45c6694a571d30f76
Seungeui Song <69485883+SeungeuiSong@users.noreply.github.com>
2023-05-10 19:41:27 -0700

    Update queue.c

--- libuthread/queue.c
+++ libuthread/queue.c
@@ -4,114 +4,97 @@
 
 #include "queue.h"
 
+struct node {
+		void* val;
+		struct node* next;
+		struct node* prev;
+};
+
 struct queue {
 	/* TODO Phase 1 */
+	int length;
+
 	struct node* head;
 	struct node* tail;
-	int number;
 };
 
 queue_t queue_create(void)
 {
 	/* TODO Phase 1 */
-	struct queue* new = malloc(sizeof(struct node));
-	new -> number = 0;
-	new -> head = NULL;
-	new -> tail = new -> head;
-	return new;
+
+	// create queue, initialize head and tail of queue
+	struct queue* queue = malloc(sizeof(struct queue));
+	struct node* head = malloc(sizeof(struct node));
+	struct node* tail = malloc(sizeof(struct node));
+
+	queue->head = head;
+	queue->tail = tail;
+	queue->length = 0;
+	
+	return queue;
 }
 
 int queue_destroy(queue_t queue)
 {
 	/* TODO Phase 1 */
-	if(queue == NULL || queue -> number !=0)
+
+	// return -1 if queue is NULL or if queue is not empty
+	if (queue->length != 0 || queue == NULL) {
 		return -1;
-	else{
-		free(queue);
-		return 0;
 	}
+
+	// deallocate memory
+	free(queue->head);
+	free(queue->tail);
+	free(queue);
+
+	return 0;
 }
 
 int queue_enqueue(queue_t queue, void *data)
 {
 	/* TODO Phase 1 */
-	if(queue == NULL || data == NULL)
+
+	// return -1 if queue or data is NULL
+	if (queue == NULL || data == NULL) {
 		return -1;
-
-    struct Node *newNode = malloc(sizeof(Node)); 
-    newNode->value = data;
-    newNode->next = NULL;
-
-	if(queue -> number == 0){
-
-		queue -> head = newNode;
 	}
-	else{
-		queue -> tail -> next = newNode;
+
+	// create new node to enqueue into queue and set data value
+	struct node* newNode = malloc(sizeof(struct node));
+	newNode->val = data;
+
+	// if queue is empty, add a node between head and tail
+	if (queue->length == 0) {
+		queue->head->next = newNode;
+		queue->tail->prev = newNode;
+		newNode->prev = queue->head;
+		newNode->next = queue->tail;
 	}
-	queue -> number ++;
-	queue -> tail = newNode;
+	// otherwise, add a node right before the tail
+	else {
+		newNode->prev = queue->tail->prev;
+		newNode->next = queue->tail;
+		queue->tail->prev->next = newNode;
+		queue->tail->prev = newNode;
+	}
+
+	// increment length
+	queue->length++;
+
 	return 0;
 }
 
 int queue_dequeue(queue_t queue, void **data)
 {
 	/* TODO Phase 1 */
-	if(queue == NULL || data == NULL || queue->number == 0)
-		return -1;
-	struct node* storehead =  queue->head;
-	*data = queue -> head -> value;
-	queue -> head = queue -> head -> next;
-	free (storehead);
-	queue -> number --;
-	return 1;
-}
 
-int queue_delete(queue_t queue, void *data)
-{
-	/* TODO Phase 1 */
-	if(queue == NULL || data == NULL)
+	// return -1 if queue or data is NULL
+	if (queue->length == 0 || queue == NULL || data == NULL) {
 		return -1;
-	struct node* search = queue -> head;
-	if( search -> value == data){
-		struct node* store =  queue->head;
-		queue -> head = queue -> head -> next;
-		free(store);
-		return 0;
 	}
-	while(search ->next != NULL){
-		if(search->next ->value == data){
-			struct node* store = search -> next;
-			search -> next = search -> next -> next;
-			free(store);
-			search -> number --;
-			return 0;			
-		}
-	}
-	return -1;
+	
+	// get second node in queue in order to make it first node after dequeueing
+	struct node* secondNode = queue->head->next->next;
 
-}
-
-int queue_iterate(queue_t queue, queue_func_t func)
-{
-	/* TODO Phase 1 */
-	if(queue == NULL || func == NULL)
-		return -1;
-	struct node* step = queue->head;
-	for(int i = 0; i < queue -> number; i++){
-		if(step != NULL){
-			func(queue, step->value);
-		}
-		step = step -> next;
-	}
-	return 0;
-}
-
-int queue_length(queue_t queue)
-{
-	/* TODO Phase 1 */
-	if(queue ==NULL)
-		return -1;
-	return queue -> number;
-}
-
+	*data = queue->head->next->val;
