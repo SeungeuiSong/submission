ff58526f1f613f10af027a1d35419b6a36753caa
Seungeui Song <69485883+SeungeuiSong@users.noreply.github.com>
2023-05-11 00:52:52 -0700

    Update uthread.c

--- libuthread/uthread.c
+++ libuthread/uthread.c
@@ -95,19 +95,13 @@ int uthread_run(bool preempt, uthread_func_t func, void *arg)
 void uthread_block(void)
 {
     preempt_disable();
-    struct uthread_tcb *prev_thread = process;
-    prev_thread->state = 0;
-    queue_enqueue(waiting_list, prev_thread);
-    queue_dequeue(ready_list, ((void**) &process));
     uthread_ctx_switch(prev_thread->context, next_thread->context);
-    preempt_enable();
+
 }
 
 void uthread_unblock(struct uthread_tcb *uthread)
 {
-    preempt_disable();
-    uthread->state = 1;
-    queue_delete(waiting_list, uthread);
+    
     queue_enqueue(ready_list, uthread);
     preempt_enable();
 }
